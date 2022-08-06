bebe07194ee9909bb82e0fb77d76485eae9058e1
Alain Igban <apigban@gmail.com>
2022-08-07 01:14:52 +0400

    Cleaned up view.go

--- views/view.go
+++ views/view.go
@@ -2,6 +2,12 @@ package views
 
 import (
 	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
 )
 
 type View struct {
@@ -12,11 +18,7 @@ type View struct {
 // NewView function parses all templates and returns a View type
 // Panics when a template cannot be used.
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml",
-	)
+	files = append(files, layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil { // Parse a view that is not present, will kill the app (panic)
@@ -27,4 +29,13 @@ func NewView(layout string, files ...string) *View {
 		Template: t,
 		Layout:   layout,
 	}
+}
+
+// layoutFiles returns a slice of strings representing the layout files used by templates
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
