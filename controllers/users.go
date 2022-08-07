50294553a528547be6c5db422a1347604412236f
Alain Igban <apigban@gmail.com>
2022-08-08 03:51:48 +0400

    Created the Create() method on the users controller
    updated routes to have the correct HTTP verbs

--- controllers/users.go
+++ controllers/users.go
@@ -1,15 +1,18 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/apigban/lenslocked_v1/views"
 )
 
 // NewUsers is used to create a new Users controller.
-// Thhis function will panic if the templates are not
+// This function will panic if the templates are not
 // parsed correctly, and should only be used during
 // initial setup.
+//
+// GET /signup
 func NewUsers() *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
@@ -24,4 +27,12 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+// Create is used to process the signup form when a user submits it.
+// this is used to create a new user account
+//
+// POST /signup
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a temporary response")
 }
