fe18baaa482033cd249e1e71aa3acdaead41000b
Alain Igban <apigban@gmail.com>
2022-08-09 12:50:24 +0400

    Created the UserService for interacting with user models

--- models/users.go
+++ models/users.go
@@ -1,9 +1,58 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+var (
+	// ErrNotFound is returned when a resource is not found in the database
+	ErrNotFound = errors.New("models: resource not found")
+)
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+//ByID will look up a user by ID provided
+// Case 1 - user, nil
+// Case 2 - nil, ErrNotFound
+// Case 3 - nil, otherError
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+//Close closes the UserService data connection
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+//DestructiveReset drops the user table and rebuilds it
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
+}
 
 type User struct {
 	gorm.Model
