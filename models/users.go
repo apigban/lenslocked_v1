8a8089046ec77520a7e1cc809bca03134d7e7399
Alain Igban <apigban@gmail.com>
2022-08-09 20:24:03 +0400

    Create() user method added to UserService

--- models/users.go
+++ models/users.go
@@ -44,6 +44,13 @@ func (us *UserService) ByID(id uint) (*User, error) {
 	}
 }
 
+// Create will create the provided user and backfill the data
+// like ID, CreatedAt and UpdatedAt
+func (us *UserService) Create(user *User) error {
+	return us.db.Create(user).Error
+	//TODO - create specific errors, like if it is invalid, or user already exists
+}
+
 //Close closes the UserService data connection
 func (us *UserService) Close() error {
 	return us.db.Close()
