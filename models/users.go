5b6fe4f7f3e8021fa8bfb457e9597534c8a46b6a
Alain Igban <apigban@gmail.com>
2022-08-09 22:09:17 +0400

    Added Error for when an invalidID is passed
    Created a method to delete a single user by ID

--- models/users.go
+++ models/users.go
@@ -10,6 +10,9 @@ import (
 var (
 	// ErrNotFound is returned when a resource is not found in the database
 	ErrNotFound = errors.New("models: resource not found")
+
+	// ErrInvalidID is returned when an invalid ID is provided to a method like Delete()
+	ErrInvalidID = errors.New("models: ID provided was invalid")
 )
 
 func NewUserService(connectionInfo string) (*UserService, error) {
@@ -49,6 +52,9 @@ func (us *UserService) ByEmail(email string) (*User, error) {
 	return &user, err
 }
 
+// first will query the provided gorm.DB and it will
+// get the first item returned and place it to dst. If
+// nothing is found the query, it will return ErrNotFound
 func first(db *gorm.DB, dst interface{}) error {
 	err := db.First(dst).Error
 	if err == gorm.ErrRecordNotFound {
@@ -62,6 +68,17 @@ func first(db *gorm.DB, dst interface{}) error {
 func (us *UserService) Create(user *User) error {
 	return us.db.Create(user).Error
 	//TODO - create specific errors, like if it is invalid, or user already exists
+}
+
+// Delete will delete the user with the provided ID
+// in the provided user object
+func (us *UserService) Delete(id uint) error {
+	if id == 0 {
+		return ErrInvalidID
+	}
+	user := User{Model: gorm.Model{ID: id}}
+	return us.db.Delete(&user).Error
+
 }
 
 // Update will update the provided user with all of the data
